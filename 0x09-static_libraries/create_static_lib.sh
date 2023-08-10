63d0c79e6b7a3d0b874bc76362c7fb7fcf878475
Glitzzybetty <130115684+Glitzzybetty@users.noreply.github.com>
2023-08-10 14:04:55 +0100

    Static libraries

---
+++ 0x09-static_libraries/create_static_lib.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# a script that creates a static library
+# called liball.a from all the .c files
+# that are in the current directory
+
+gcc -Wall -Wextra -Werror -pedantic -c *.c
+ar rc liball.a *.o
