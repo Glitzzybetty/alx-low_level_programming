587315c5f557a28da20f171a41d0784729adae19
Glitzzybetty <omotayo.ikudayisi@gmail.com>
2023-08-28 07:34:46 -0700

    a function that adds a new node at the beginning of a list

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -17,6 +17,11 @@ if (new_node == NULL)
 	return (NULL);
 }
 new_node->str = strdup(str);
+if  (new_node->str == NULL)
+{
+	free(new_node);
+	return (NULL);
+}
 new_node->next = *head;
 *head = new_node;
 return (new_node);
